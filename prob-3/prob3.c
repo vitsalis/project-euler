d0b013c2f01288c1d14600d862c5188878317c1c
VitSalis <vitsalis@gmail.com>
2012-08-28 12:36:41 +0300

    Adding my project euler solutions to github

--- prob-3/prob3.c
+++ prob-3/prob3.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int main ( )
-{
+int main (){
+
 	long long int super = 600851475143;
 	int i = 2, j = 1;
 	int prevnum = 0, num = 0;
 
-	while(j){
-		if(super % i == 0.0){
+	while( j ){
+		if( super % i == 0.0 ){
 			super = super / i;
 			prevnum = i;
-			if(prevnum > num){
+			if( prevnum > num ){
 				num = prevnum;
 			}
 			i = 2;
@@ -18,7 +18,7 @@ int main ( )
 		else{
 			i++;
 		}
-		if(super < num){
+		if( super < num ){
 			printf("%d\n",num);
 			j = 0;
 		}
--- prob-6/prob6.c
+++ prob-6/prob6.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int square(int );
-int main ()
-{
+int main(){
+
 	int tot1, tot2, dif, num1, num2;
 	num1 = num2 = tot1 = tot2 = dif = 0;
-	while(num1 <= 100){
-		tot1 += square(num1);
-		num1++;
+	while( num1 <= 100 ){
+		tot1 += square( num1 );
+		++num1;
 	}
 	
-	while(num2 <= 100){
+	while( num2 <= 100 ){
 		tot2 += num2;
-		num2++;
+		++num2;
 	}
-	tot2 = square(tot2);
+	tot2 = square( tot2 );
 
 	dif = tot2 - tot1;
-	printf("%d\n", dif);
+	printf( "%d\n", dif );
 	return 0;
 }
 
