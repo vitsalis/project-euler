f3dfbd700f485d9c542946ede29d1ba4a0138c76
Vitalis Salis <vitsalis@gmail.com>
2015-06-11 02:54:57 +0300

    Add solution to problem 7 in python

---
+++ prob-7/prob7.py
@@ -0,0 +1,16 @@
+def isPrime(num):
+    """Checks if the number is a prime"""
+    isPrime = True
+    for i in range(2, num - 1):
+        if num % i == 0:
+            isPrime = False
+            break
+    return isPrime
+cnt = 0
+i = 2
+
+while cnt < 10001:
+    if isPrime(i):
+        cnt += 1
+    i += 1
+print(i - 1)
