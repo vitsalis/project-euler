970f950d4a866df0cb5cfc8292a061564527a07d
Vitalis Salis <vitsalis@gmail.com>
2015-06-11 17:06:20 +0300

    Use filter and make code more readable

--- prob-10/prob10.py
+++ prob-10/prob10.py
@@ -1,12 +1,10 @@
 def is_prime(num):
     """Checks if the given number is a prime"""
-    is_prime = True
     for i in range(2, num // 2 + 1):
         if num % i == 0:
-            is_prime = False
-            break
-    return is_prime
+            return False
+    return True
 
-primes = [x for x in range(2, 2 * 10 ** 6) if is_prime(x)]
+primes = filter(is_prime, [x for x in range(2, 2 * 10 ** 6)])
 
 print sum(primes)
--- prob-3/prob3.py
+++ prob-3/prob3.py
@@ -1,12 +1,10 @@
-max = 1
 num = 600851475143
 
-for i in range(3, 10 ** 7):
+for i in range(10 ** 7, 3, -1):
     if num % i == 0:
         for j in range(2, i // 2 + 1):
             if i % j == 0:
                 break
         else:
-            max = i
-
-print max
+            print i
+            break
